5ce644fa6588ee68121574e6f8fc52b42af65809
daniel <danielvolosov@outlook.com>
2021-03-05 10:15:29 -0500

    Final program functionality added, writing tests

--- src/main/java/solution/CommonTermCounter.java
+++ src/main/java/solution/CommonTermCounter.java
@@ -6,20 +6,20 @@ import java.util.stream.Collectors;
 
 public class CommonTermCounter {
 
-    public Map<String, Long> getCommonWordFrequencyAndCount(List<String> stems) {
+    public List<Map.Entry<String, Long>> getCommonWordFrequencyAndCount(List<String> stems) {
 
         // Add freq to map
         final Map<String, Long> collect = stems.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-// Sort and map 20 words
+        // Sort and map 20 words
         final List<Map.Entry<String, Long>> entries = new ArrayList<>(collect.entrySet());
         final List<Map.Entry<String, Long>> result = entries.stream()
                 .sorted(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()))
                 .limit(20)
                 .collect(Collectors.toList());
 
-        return (Map<String, Long>) result;
+        return result;
     }
 }
 
--- src/main/java/solution/Main.java
+++ src/main/java/solution/Main.java
@@ -19,9 +19,7 @@ public class Main {
         List<String> stemsOfWords = textStemmer.stemWords(fileWithoutStopwords);
 
         System.out.println("This output represents the words in the input text file without stop words & without non-alphabetical text: " + fileWithoutStopwords);
-//
-//        System.out.println("This output represents the previous output, but all the words are stemmed into their root form: " + stemsOfWords);
-
-//        System.out.println("This output represents the top-20 most commonly occuring terms, as well as the frequency of each term: " + CommonTermCounter.getCommonWordFrequencyAndCount(stemsOfWords));
+        System.out.println("This output represents the previous output, but all the words are stemmed into their root form: " + stemsOfWords);
+        System.out.println("This output represents the top-20 most commonly occuring stems, as well as the frequency of each stem: " + commonTermCounter.getCommonWordFrequencyAndCount(stemsOfWords));
     }
 }
